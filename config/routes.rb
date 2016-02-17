4dba06d63875aaac184184cb1de0c960621111e5
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-02-17 16:14:08 +0530

    Revised README.MD

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index', as: :landing
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
+  post 'contact' => 'home#test', as: :contacts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
