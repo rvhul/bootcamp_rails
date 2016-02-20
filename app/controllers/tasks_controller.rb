69fa7408ebe00c72fde59cd2f36dabb25c9d04f2
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-02-20 13:00:17 +0530

    Add new task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
+
   def index
+
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
 
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+def seed_task(title)
+  Task.find_or_create_by(title: title)
+  print "."
+end
+
+puts "# Seeding tasks"
+seed_task("Eat")
+seed_task("Jam")
+seed_task("Smash")
+seed_task("Sleep")
+puts "   Done"
