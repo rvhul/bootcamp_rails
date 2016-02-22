e6ce9a839b357705889dc15ee8119d733ebd38ca
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-02-22 18:28:03 +0530

    Add secure password migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222091952) do
+ActiveRecord::Schema.define(version: 20160222125626) do
 
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -29,8 +29,11 @@ ActiveRecord::Schema.define(version: 20160222091952) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
