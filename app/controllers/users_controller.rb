5a5ec1569d5bd429bd405370f01aed9741a43f4d
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-02-23 11:47:09 +0530

    Add strong user params for creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[user_params])
+      if @user.save
+        ###
+      else
+        render 'new'
+      end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
