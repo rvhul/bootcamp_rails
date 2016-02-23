37071319b88c707dddf656551608c873d814670e
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-02-23 15:09:59 +0530

    Add integration test for potential invalid signup information entered

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup info" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: {name: "", email: "hey@there.com", password:"foo", password_confirmation:"bar"}
+    end
+    assert_template 'users/new'
+  end
+end
