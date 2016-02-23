b164118f5f913ca7320e74b6bc9d1b14e46879ad
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-02-23 16:00:00 +0530

    Sign up page functional to add user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[user_params])
+    @user = User.new(user_params)
       if @user.save
-        ###
+        flash[:success] = "Welcome yo!"
+        redirect_to @user
       else
         render 'new'
       end
